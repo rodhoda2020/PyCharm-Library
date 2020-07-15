2b5d4f9b1256cc6f9c3d7e366890e8b3aef639ab
rodhoda2020 <rod.hoda@gmail.com>
2020-07-14 22:40:54 -0700

    Added the most recent code.

--- Python_Data_Science_Handbook/Pandas/Data_Manipulation_with_Pandas.py
+++ Python_Data_Science_Handbook/Pandas/Data_Manipulation_with_Pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def Introducing_Pandas_Objects():
 
@@ -75,7 +76,154 @@ def Introducing_Pandas_Objects():
 
     # Constructing Series objects
 
+    # We've already seen a few ways of constructing a Pandas Series from scratch;
+    # all of them are some version of the following:
+    # pd.Series(data, index=index)
 
+    # where index is an optional argument, and data can be one of many entities
+
+    # For example, it can be a list or NumPy array, in which the index defaults
+    # to an integer sequence:
+    print(pd.Series([2, 4, 6]))
+
+    # Data can be scalar
+    pd.Series(5, index=[100, 200, 300])
+
+    # Data can be a dictionary, in which index defaults to the sorted dictionary
+    # keys:
+    print(pd.Series({2:'a', 1:'b', 3:'c'}))
+
+    # The Pandas DataFrame Object
+
+    # The next fundamental structure is the DataFrame. Like the Series object,
+    # the DataFrame can be thought of either as a generalization of a NumPy
+    # array, or as a specialization of a Python dictionary.
+
+    # DataFrame as a generalized NumPy array
+
+    # If a series is an analog of a one-dimensional array with flexible indices,
+    # a DataFrame is an analog of a two-dimensional array with both flexible
+    # row indices and flexible column names.
+
+    # You can think of a DataFrame as a sequence of aligned Series objects.
+
+    # To demonstrate this, let's first construct a new Series
+    area_dict = {'California': 423967, 'Texas': 695662, 'New York': 141297,
+                 'Florida': 170312, 'Illinois': 149995}
+
+    area = pd.Series(area_dict)
+    print(area)
+
+    # Now that we have this and the population series from before, we can use
+    # them to construct a single two-dimensional object
+    states = pd.DataFrame({'population': pop,
+                           'area': area})
+    print(states)
+
+    # Like the Series object, the DataFrame has an index attribute that gives
+    # access to the index labels:
+    print(states.index)
+
+    # Additionally, the DataFrame has a columns attribute, which is an index
+    # object holding the column labels:
+    print(states.columns)
+
+    # DataFrame as specialized dictionary
+
+    # Similary, we can also think of a DataFrame as a specialization of a
+    # dictionary. Where a dictionary maps a key to a value, a DataFrame maps
+    # a column name to a Series of column data.
+
+    # For example, asking for the 'area' attribute returns the Series object
+    # containing the areas we saw earlier:
+    print(states['area'])
+
+    # Notice the potential point of confusion here: in a two-dimensional NumPy
+    # array, data[0] will return the first row. For a DataFrame, data['col0']
+    # will return the first column.
+
+    # Constructing DataFrame objects
+
+    # A DataFrame can be constructed in a variety of ways:
+
+    # 1. From a single Series object
+    pd.DataFrame(pop, columns=['population'])
+
+    # 2. From a list of dicts
+    data = [{'a': i, 'b': 2*i}
+            for i in range(3)]
+    print(pd.DataFrame(data))
+
+    # Even if some keys are missing, Pandas will fill them in with NaN
+    pd.DataFrame([{'a': 1, 'b':2}, {'b': 3, 'c': 4}])
+
+    # 3. From a dictionary of Series objects
+    pd.DataFrame({'population': pop,
+                  'area': area})
+
+    # 4. From a two-dimensional NumPy array.
+    example = pd.DataFrame(np.random.rand(3, 2),
+                           columns=['foo', 'bar'],
+                           index=['a', 'b', 'c'])
+    print(example)
+
+    # 5. From a NumPy structured array
+    A = np.zeros(3, dtype=[('A', 'i8'), ('B', 'f8')])
+
+    print(A.dtype)
+
+    print(pd.DataFrame(A))
+
+    # The Pandas Index Object
+
+    # We have seen here that both the Series and DataFrame objects contain
+    # an explicit index that lets you reference and modify data. This index
+    # object is an interesting structure in itself, and it can be thought
+    # of as immutable array or as an ordered set.
+
+    # Those views have some interesting consequences in the operations available
+    # on Index objects. Here is an example:
+    ind = pd.Index([2, 3, 5, 7, 11])
+    print(ind)
+
+    # Index as immutable array
+
+    # The Index object in many ways operates like an array. For example, we can
+    # use Python indexing notation to retrieve values or slices:
+    print(ind[1])
+
+    print(ind[::2])
+
+    # Index objects also have many of the attributes familiar from NumPy arrays:
+    print(ind.size, ind.shape, ind.ndim, ind.dtype)
+
+    # One difference between Index objects and NumPy arrays is that indices
+    # are immutable - that is, they cannot be modified via the normal means:
+    # ind[1] = 0    = error
+
+    # Index as ordered set
+
+    # Pandas objects are designed to facilitate operations such as joins across
+    # datasets, which depend on many aspects of set arithmetic.
+
+    indA = pd.Index([1, 3, 5, 7, 9])
+    indB = pd.Index([2, 3, 5, 7, 11])
+
+    # Intersection
+    print(indA & indB)
+
+    # Union
+    print(indA | indB)
+
+    # Symmetric difference
+    print(indA ^ indB)
+
+    # You can also write these operations via object methods
+    indA.intersection(indB)
+
+def Data_Indexing_and_Selection():
+    print("Implement!")
 
 if __name__ == "__main__":
-    Introducing_Pandas_Objects()
+    # Introducing_Pandas_Objects()
+    Data_Indexing_and_Selection()
