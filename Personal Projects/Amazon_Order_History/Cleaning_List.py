37419af516b6771a7e8093bfe19c0964a2d69137
Rod Hoda <rod.hoda12live.co.uk@gmail.com>
2021-03-02 18:28:11 -0800

    Added the most recent code

---
+++ Personal Projects/Amazon_Order_History/Cleaning_List.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("01-Jan-2018_to_24-Feb-2021.csv")
+
+pd.set_option('display.max_columns', None)
+
+to_drop = ['Order ID',
+           'UNSPSC Code',
+           'ASIN/ISBN',
+           'Seller Credentials',
+           'Purchase Order Number',
+           'PO Line Number',
+           'Tax Exemption Applied',
+           'Tax Exemption Type',
+           'Exemption Opt-Out',
+           'Group Name',
+           'Release Date',
+           'Seller',
+           'Carrier Name & Tracking Number']
+
+df.drop(to_drop, inplace=True, axis=1)
+
+print(df.head())
+
+# A Calendar heatmap that shows the number of purchases made in a single day based on the color of the block (day)
+
+
+# What were the purchase price ranges?
+
+# The number of purchases made by each person
+
+# THe amount of money spent by each person
+
+# How many purchases have been made between the two addresses
+
+# How many days passed from the purchase date to shipment date
+
+# What are the most common average days passed from purchase to shipment date
