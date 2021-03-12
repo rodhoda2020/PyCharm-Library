bf3dbe6b17cc260731cd75a4a53233c617ab15c0
rodhoda <rod.hoda12live.co.uk@gmail.com>
2021-03-11 16:58:19 -0800

    Added the most recent code

--- Personal Projects/Guessing_Game/app.py
+++ Personal Projects/Guessing_Game/app.py
@@ -1,26 +1,54 @@
+import random
 from random import randint
 
 
 class Game(object):
 
-    def __init__(self, s_range, e_range):
-        self.s_range = s_range
-        self.e_range = e_range
+    answer = 0
+    guess = 0
+    count = 0
+    win = False
+    current_score = 0
+    total_score = 0
+
+    def __init__(self, start_range, end_range):
+        self.s_range = start_range
+        self.e_range = end_range
 
     @staticmethod
     def random_generator():
-        answer = randint(int(s_range), int(e_range))
-        # print('The answer is {}'.format(answer))
-        return answer
+        answer = random.randint(int(s_range), int(e_range))
+        print('The answer is {}'.format(Game.answer))
+        return Game.answer
 
     @staticmethod
     def score_system(s):
-        score = s
-        score += (s_range - e_range)
-        return score
+        Game.current_score = s
+        Game.current_score += (s_range - e_range)
 
-    def guess_game(self):
-        return
+
+    @staticmethod
+    def guess_game():
+        if Game.guess == Game.answer:
+            print("Hurray, you guessed the correct number!")
+        elif Game.guess > Game.answer:
+            print("It's less than that.")
+        else:
+            print("It's more than that.")
+
+    def game_system(self):
+        Game.guess = int(input('The number is between {}-{}. \nGuess the number: '.format(s_range, e_range)))
+        # print(guess)
+        while Game.win == False:
+            if Game.guess == Game.answer:
+                print("Congratulations! You guessed the right number after {} tries.".format(Game.count))
+                Game.Win = True
+            else:
+                self.guess_game()
+                Game.count += 1
+
+
+
 
 
 if __name__ == '__main__':
@@ -35,3 +63,4 @@ if __name__ == '__main__':
 
     g = Game(s_range, e_range)
     g.random_generator()
+    g.guess_game()
